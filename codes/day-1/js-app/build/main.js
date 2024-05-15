63d4e7b93aca2ee36bee0c9af19071d0cf7e4496
Joydip <joydip.mondal@gmail.com>
2024-05-15 12:18:38 +0530

    day-1 pre-lunch update

---
+++ codes/day-1/js-app/build/main.js
@@ -0,0 +1,9 @@
+"use strict";
+function subtract(a, b) {
+    return (a - b);
+}
+function multiply(a, b) {
+    return a * b;
+}
+console.log(subtract(2, 1));
+console.log(multiply(12, 3))
---
+++ codes/day-1/js-app/src/main.ts
@@ -0,0 +1,7 @@
+function subtract(a: number, b: number) {
+    return (a - b)
+}
+function multiply(a: number, b: number) {
+    return a * b
+}
+console.log(subtract(2, 1))
